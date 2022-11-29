fa0eb0c32ee3c7cb59598ab69d4d74e6811fb437
TigranAvushyan <tigran.avushyan@gmail.com>
2022-11-29 09:13:55 +0300

    feat: prettier init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.scss'
-import { Articles } from './pages/articles'
-import { History } from './pages/history'
-import { Home } from './pages/home'
-import { Prescription } from './pages/prescription'
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import { Articles } from './pages/articles';
+import { History } from './pages/history';
+import { Home } from './pages/home';
+import { Prescription } from './pages/prescription';
 
 function App() {
-
   return (
-    <div className="App">
-
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/history' element={<History />}/>
-      <Route path='/prescription' element={<Prescription />} />
-      <Route path='/articles' element={<Articles />}/>
-    </Routes>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/history' element={<History />} />
+        <Route path='/prescription' element={<Prescription />} />
+        <Route path='/articles' element={<Articles />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
