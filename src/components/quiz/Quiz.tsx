1fd0ff4f73adc4b1d5f994b83a73172abfd2962a
kotiksmelyi <julynish@mail.ru>
2022-12-01 17:08:23 +0300

    add datepicker, prescriptiondata

--- src/components/quiz/Quiz.tsx
+++ src/components/quiz/Quiz.tsx
@@ -1,13 +1,13 @@
 import { BodyHeader } from '@components/body/BodyHeader';
-import { SwitchFormFields } from '@components/quiz/SwitchFormFields';
 
 import { useMultiStepFormNavigation } from '@hooks/useMultiStepFormNavigation';
 
 import { quizData } from '@utils/quiz/testData';
 
-import { Button, Checkbox, Form, Input, Radio } from 'antd';
+import { Button, Checkbox, Form, Radio } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const questionHeader = (
   name: string,
@@ -23,63 +23,80 @@ export const Quiz: FC = () => {
 
   const finishForm = (value: any) => {
     console.log('values', value);
+    setFinished(true);
   };
   const currentQuestion = quizData.questions[activePage];
 
+  const [finished, setFinished] = useState(false);
+
   return (
     <>
-      <BodyHeader
-        text={questionHeader(
-          quizData.name,
-          activePage + 1,
-          quizData.questions.length
-        )}
-      />
-
-      <Form
-        name='basic'
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        onFinish={finishForm}
-        autoComplete='off'
-      >
-        <div className='select'>
-          <h2>{currentQuestion?.text}</h2>
-          <div className='select__container'>
-            <Form.Item name={'text'}>
-              {currentQuestion.type === 'select' && (
-                <Checkbox.Group
-                  options={currentQuestion.options.map(
-                    (option: any) => option.text
+      {!finished && (
+        <>
+          <BodyHeader
+            text={questionHeader(
+              quizData.name,
+              activePage + 1,
+              quizData.questions.length
+            )}
+          />
+          <Form
+            name='basic'
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={finishForm}
+            autoComplete='off'
+          >
+            <div className='select'>
+              <h2>{currentQuestion?.text}</h2>
+              <div className='select__container'>
+                <Form.Item name={'text'}>
+                  {currentQuestion.type === 'select' && (
+                    <Checkbox.Group
+                      options={currentQuestion.options.map(
+                        (option: any) => option.text
+                      )}
+                    />
                   )}
-                />
-              )}
 
-              {currentQuestion.type === 'multiselect' && (
-                <Radio.Group>
-                  {currentQuestion.options.map((question: any) => (
-                    <Radio key={question.id} value={question.id}>
-                      {question.text}
-                    </Radio>
-                  ))}
-                </Radio.Group>
-              )}
+                  {currentQuestion.type === 'multiselect' && (
+                    <Radio.Group>
+                      {currentQuestion.options.map((question: any) => (
+                        <Radio key={question.id} value={question.id}>
+                          {question.text}
+                        </Radio>
+                      ))}
+                    </Radio.Group>
+                  )}
 
-              {currentQuestion.type === 'textarea' && (
-                <TextArea
-                  rows={5}
-                  maxLength={250}
-                  placeholder={'Максимум символов: 250'}
-                />
-              )}
-            </Form.Item>
+                  {currentQuestion.type === 'textarea' && (
+                    <TextArea
+                      rows={5}
+                      maxLength={250}
+                      placeholder={'Максимум символов: 250'}
+                    />
+                  )}
+                </Form.Item>
+              </div>
+            </div>
+            {!isFirstPage && <Button onClick={goPrev}>{'Назад'}</Button>}
+            {!isLastPage && <Button onClick={goNext}>Дальше</Button>}
+            {isLastPage && <Button htmlType='submit'>Отправить</Button>}
+          </Form>
+        </>
+      )}
+      {finished && (
+        <>
+          <BodyHeader text={'Анкета отправлена!'} />
+          <div className='finished'>
+            <h1>Спасибо за заполнение анкеты!</h1>
+            <Link className='button' to='/'>
+              Вернуться на главную
+            </Link>
           </div>
-        </div>
-        {!isFirstPage && <Button onClick={goPrev}>{'Назад'}</Button>}
-        {!isLastPage && <Button onClick={goNext}>Дальше</Button>}
-        {isLastPage && <Button htmlType='submit'>Отправить</Button>}
-      </Form>
+        </>
+      )}
     </>
   );
 };
