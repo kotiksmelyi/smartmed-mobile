c9c9df9acb79cce674e38f8c252529330026dfff
kotiksmelyi <julynish@mail.ru>
2022-12-03 17:42:11 +0300

    add links in history buttons

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -4,6 +4,7 @@ import { Header } from '@components/header/Header';
 
 import { historyData } from './historyData';
 import { DatePicker } from 'antd';
+import { Link } from 'react-router-dom';
 
 export function History() {
   const { RangePicker } = DatePicker;
@@ -19,20 +20,24 @@ export function History() {
         {historyData.map((note) => (
           <>
             <p>{note.date}</p>
-            <div className='select__container' key={note.id}>
-              {note.recommendations ? (
-                <h1>Рекомендации выполнены</h1>
-              ) : (
-                <h1>Рекомендации не выполнены</h1>
-              )}
-            </div>
-            <div className='select__container'>
-              {note.quiz ? (
-                <h1>Анкета заполнена</h1>
-              ) : (
-                <h1>Анкета не заполнена</h1>
-              )}
-            </div>
+            <Link to={'/prescription'}>
+              <div className='select__container' key={note.id}>
+                {note.recommendations ? (
+                  <h1>Рекомендации выполнены</h1>
+                ) : (
+                  <h1>Рекомендации не выполнены</h1>
+                )}
+              </div>
+            </Link>
+            <Link to={'/quiz'}>
+              <div className='select__container'>
+                {note.quiz ? (
+                  <h1>Анкета заполнена</h1>
+                ) : (
+                  <h1>Анкета не заполнена</h1>
+                )}
+              </div>
+            </Link>
           </>
         ))}
       </div>
