99d1fef41fc9bda5a3655385ff30169628a3c34c
TigranAvushyan <tigran.avushyan@gmail.com>
2022-12-06 12:13:59 +0300

    fix: prettier

--- src/components/body/BodyHeader.tsx
+++ src/components/body/BodyHeader.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 export function BodyHeader(headerText: { text: string }) {
+  const [asf, setAsf] = useState<unknown>();
   return (
     <div className='bodyHeader'>
       <h3>{headerText.text}</h3>
