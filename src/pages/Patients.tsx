62bc1008ece1914c446cb89d85f1ec4363a55f0f
TigranAvushyan <tigran.avushyan@gmail.com>
2022-12-08 18:15:26 +0300

    fix: conflict

--- src/pages/Patients.tsx
+++
@@ -1,9 +0,0 @@
-import { HeatMap } from '@components/heat-map/HeatMap';
-
-import testData from '@utils/heat-map/heatMapTest.json';
-
-import { FC } from 'react';
-
-export const Patients: FC = () => {
-  return <HeatMap data={testData} />;
-};
